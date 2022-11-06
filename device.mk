7f84cd4179b2b0bd41aedfe234ed5bef626d7c62
johnmart19 <johnivan19999@gmail.com>
2022-11-06 06:04:07 +0200

    Import Dolby Config

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ PRODUCT_SHIPPING_API_LEVEL := 28
 # Dolby Support
 TARGET_USES_MIUI_DOLBY := true
 
+# Dolby Config File
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dolby/config/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
