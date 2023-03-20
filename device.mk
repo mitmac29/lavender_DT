1812f37d3e6f957757305e3542cd9f4dd8b2cd57
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2023-03-20 16:11:45 +0530

    Halsss

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += device/xiaomi/lavender \
                             hardware/google/interfaces \
-                            hardware/google/pixel
+                            hardware/google/pixel \
+                            hardware/qcom-caf/msm8998
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
