3d0b17f5b8e14d6f762826172788d254474c5bb0
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2023-05-21 15:47:45 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lavender.mk
+    $(LOCAL_DIR)/aicp_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lavender-user \
-    aosp_lavender-userdebug \
-    aosp_lavender-eng
+    aicp_lavender-user \
+    aicp_lavender-userdebug \
+    aicp_lavender-eng
